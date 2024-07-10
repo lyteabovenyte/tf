2cf285e6489459af833f2770d25525af256f09fd
lyteabovenyte <lyteabovenyte@gmail.com>
2024-07-10 13:32:56 +0330

    reorganize and separate live and modules folder

--- modules/services/webserver-cluster/outputs.tf
+++ modules/services/webserver-cluster/outputs.tf
@@ -8,6 +8,7 @@ output "asg_name" {
     description = "the name of the Auto Scaling Group"
 }
 
+# exporting the security group id for the load balancer.
 output "alb_security_group_id" {
     value = aws_security_group.alb.id
     description = "the ID of the security group attached to the load balancer"
